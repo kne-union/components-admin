be2ce972a5995b8184f76fe475fcfd397cbabe64
Linzp <sunandmoon001@163.com>
2026-01-07 18:36:57 +0800

    提交一下

--- src/components/BizUnit/Actions/SetStatus.js
+++ src/components/BizUnit/Actions/SetStatus.js
@@ -1,33 +1,38 @@
 import { createWithRemoteLoader } from '@kne/remote-loader';
 import merge from 'lodash/merge';
 import { App } from 'antd';
+import { useIntl } from '@kne/react-intl';
+import withLocale from '../withLocale';
 
 const SetStatus = createWithRemoteLoader({
   modules: ['components-core:LoadingButton', 'components-core:ConfirmButton', 'components-core:Global@usePreset']
-})(({ remoteModules, data, onSuccess, apis, options, getFormInner, ...props }) => {
-  const [LoadingButton, ConfirmButton, usePreset] = remoteModules;
-  const { ajax } = usePreset();
-  const { message } = App.useApp();
-  const CurrentButton = props.message || props.confirm ? ConfirmButton : LoadingButton;
-  return (
-    <CurrentButton
-      {...merge({}, props, data.status === 'open' ? options.closeButtonProps : options.openButtonProps)}
-      onClick={async () => {
-        const { data: resData } = await ajax(
-          typeof apis.setStatus === 'function'
-            ? apis.setStatus({ data, options })
-            : merge({}, apis.setStatus, {
-                data: { id: data.id, status: data.status === 'open' ? 'closed' : 'open' }
-              })
-        );
-        if (resData.code !== 0) {
-          return;
-        }
-        message.success(`修改${options.bizName}成功`);
-        onSuccess && onSuccess();
-      }}
-    />
-  );
-});
+})(
+  withLocale(({ remoteModules, data, onSuccess, apis, options, getFormInner, ...props }) => {
+    const [LoadingButton, ConfirmButton, usePreset] = remoteModules;
+    const { ajax } = usePreset();
+    const { message } = App.useApp();
+    const { formatMessage } = useIntl();
+    const CurrentButton = props.message || props.confirm ? ConfirmButton : LoadingButton;
+    return (
+      <CurrentButton
+        {...merge({}, props, data.status === 'open' ? options.closeButtonProps : options.openButtonProps)}
+        onClick={async () => {
+          const { data: resData } = await ajax(
+            typeof apis.setStatus === 'function'
+              ? apis.setStatus({ data, options })
+              : merge({}, apis.setStatus, {
+                  data: { id: data.id, status: data.status === 'open' ? 'closed' : 'open' }
+                })
+          );
+          if (resData.code !== 0) {
+            return;
+          }
+          message.success(formatMessage({ id: 'ModifySuccess' }, { bizName: options.bizName }));
+          onSuccess && onSuccess();
+        }}
+      />
+    );
+  })
+);
 
 export default SetStatus;
--- src/components/BizUnit/locale/zh-CN.js
+++ src/components/BizUnit/locale/zh-CN.js
@@ -5,7 +5,21 @@ const locale = {
   Open: '开启',
   Close: '关闭',
   Operation: '操作',
-  Keyword: '关键字'
+  Keyword: '关键字',
+  AddBiz: '添加{bizName}',
+  EditBiz: '编辑{bizName}',
+  DeleteBiz: '删除{bizName}',
+  OpenBiz: '开启{bizName}',
+  CloseBiz: '关闭{bizName}',
+  AddSuccess: '添加{bizName}成功',
+  EditSuccess: '修改{bizName}成功',
+  DeleteSuccess: '删除{bizName}成功',
+  OpenSuccess: '开启{bizName}成功',
+  CloseSuccess: '关闭{bizName}成功',
+  ConfirmDelete: '确定要删除{bizName}吗？',
+  ConfirmClose: '确定要关闭{bizName}吗？',
+  SaveSuccess: '保存{bizName}成功',
+  ModifySuccess: '修改{bizName}成功'
 };
 
 export default locale;
