2c499b67d6e669b353ca5a4e9ef608c1a030ab82
Linzp <sunandmoon001@163.com>
2026-05-09 17:54:28 +0800

    修改到处组件

--- src/components/Account/index.js
+++ src/components/Account/index.js
@@ -3,6 +3,7 @@ export { default as Login } from './Login';
 export { default as Register } from './Register';
 export { default as Modify } from './Modify';
 export { default as ForgetByEmail } from './ForgetByEmail';
+export {default as Language} from './Language';
 export { default as ResetPassword } from './ResetPassword';
 export { default as Logout, useLogout } from './Logout';
 export { default } from './Account';
