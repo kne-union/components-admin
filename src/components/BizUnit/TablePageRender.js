ca2dcd7359ac6a62111d7b3bdcb9d365fa0fba56
Linzp <sunandmoon001@163.com>
2026-07-13 15:27:30 +0800

    提交

--- src/components/BizUnit/TablePageRender.js
+++ src/components/BizUnit/TablePageRender.js
@@ -2,13 +2,18 @@ import { createWithRemoteLoader } from '@kne/remote-loader';
 import merge from 'lodash/merge';
 
 const TablePageRender = createWithRemoteLoader({
-  modules: ['components-core:Layout@TablePage', 'components-core:TablePage']
-})(({ remoteModules, titleExtra, tableOptions, page }) => {
-  const [LayoutTablePage, NextTablePage] = remoteModules;
+  modules: ['components-core:Layout@TablePage', 'components-core:Table@TablePage', 'components-core:TablePage']
+})(({ remoteModules, titleExtra, tableOptions, page, withPage = true }) => {
+  const [LayoutTablePage, LegacyTablePage, NextTablePage] = remoteModules;
 
   if (tableOptions?.isNext) {
     const { page: layoutPage, isNext, ...nextTableOptions } = tableOptions;
     return <NextTablePage {...nextTableOptions} />;
+  }
+
+  // SystemLayout / 外层已包 Page 时不要再套 Layout@TablePage，否则会调用不存在的 setPageProps
+  if (!withPage) {
+    return <LegacyTablePage {...tableOptions} />;
   }
 
   return (
