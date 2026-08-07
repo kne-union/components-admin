7fb3a02e3a32fcad12d64bd4aae16594b5e7b5f5
Linzp <sunandmoon001@163.com>
2026-08-07 11:14:59 +0800

    提交

--- src/preset.js
+++ src/preset.js
@@ -57,7 +57,7 @@ export const globalInit = async () => {
     //url: 'http://localhost:3001',
     //tpl: '{{url}}',
     remote: 'components-core',
-    defaultVersion: '0.5.26'
+    defaultVersion: '0.5.33'
   };
   remoteLoaderPreset({
     remotes: {
